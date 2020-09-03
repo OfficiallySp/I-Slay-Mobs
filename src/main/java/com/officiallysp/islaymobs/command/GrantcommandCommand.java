dd2a2c2cac718e2198b3451666f7a62c109406f0
OfficiallySp777 <shane@192.168.1.223>
2020-09-03 18:32:59 +0100

    Readded JEI API

--- src/main/java/com/officiallysp/islaymobs/command/GrantcommandCommand.java
+++ src/main/java/com/officiallysp/islaymobs/command/GrantcommandCommand.java
@@ -1,9 +1,27 @@
 
 package com.officiallysp.islaymobs.command;
 
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.common.util.FakePlayerFactory;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.command.Commands;
+import net.minecraft.command.CommandSource;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+
+import com.officiallysp.islaymobs.procedures.CommandExecutedProcedure;
+import com.officiallysp.islaymobs.ISlayMobsModElements;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.arguments.StringArgumentType;
+
 @ISlayMobsModElements.ModElement.Tag
 public class GrantcommandCommand extends ISlayMobsModElements.ModElement {
-
 	public GrantcommandCommand(ISlayMobsModElements instance) {
 		super(instance, 9);
 	}
@@ -20,15 +38,12 @@ public class GrantcommandCommand extends ISlayMobsModElements.ModElement {
 
 	private int execute(CommandContext<CommandSource> ctx) {
 		ServerWorld world = ctx.getSource().getWorld();
-
 		double x = ctx.getSource().getPos().getX();
 		double y = ctx.getSource().getPos().getY();
 		double z = ctx.getSource().getPos().getZ();
-
 		Entity entity = ctx.getSource().getEntity();
 		if (entity == null)
 			entity = FakePlayerFactory.getMinecraft(world);
-
 		HashMap<String, String> cmdparams = new HashMap<>();
 		int[] index = {-1};
 		Arrays.stream(ctx.getInput().split("\\s+")).forEach(param -> {
@@ -36,16 +51,11 @@ public class GrantcommandCommand extends ISlayMobsModElements.ModElement {
 				cmdparams.put(Integer.toString(index[0]), param);
 			index[0]++;
 		});
-
 		{
 			Map<String, Object> $_dependencies = new HashMap<>();
-
 			$_dependencies.put("entity", entity);
-
 			CommandExecutedProcedure.executeProcedure($_dependencies);
 		}
-
 		return 0;
 	}
-
 }
