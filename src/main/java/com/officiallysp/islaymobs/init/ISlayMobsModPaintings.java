cdd269c26f1bb0188a78497be986529945550940
OfficiallySp <shane.pepperell@me.com>
2024-03-06 11:43:58 +0000

    added cheddar

--- src/main/java/com/officiallysp/islaymobs/init/ISlayMobsModPaintings.java
+++ src/main/java/com/officiallysp/islaymobs/init/ISlayMobsModPaintings.java
@@ -15,4 +15,5 @@ import com.officiallysp.islaymobs.ISlayMobsMod;
 public class ISlayMobsModPaintings {
 	public static final DeferredRegister<PaintingVariant> REGISTRY = DeferredRegister.create(ForgeRegistries.PAINTING_VARIANTS, ISlayMobsMod.MODID);
 	public static final RegistryObject<PaintingVariant> CHEESELOGO = REGISTRY.register("cheeselogo", () -> new PaintingVariant(32, 32));
+	public static final RegistryObject<PaintingVariant> CHEDDAR = REGISTRY.register("cheddar", () -> new PaintingVariant(32, 32));
 }
